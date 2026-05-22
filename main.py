f5abb51c10d3d2319be3bdb700bb7f8e63599e2f
Rohit Bhartiya <rbhartiya895.com>
2026-05-22 16:57:00 +0530

    Day 7: Added assistant states, wake/sleep system, shutdown lifecycle, and stability improvements

--- main.py
+++ main.py
@@ -6,24 +6,108 @@ from voice.listen import listen
 from voice.speak import speak
 
 from core.router import route_command
-from core.state_manager import assistant_running
+
+from core.state_manager import (
+    get_state,
+    is_active,
+    is_sleeping,
+    is_shutdown,
+    set_state,
+    ACTIVE
+)
 
 
 print("RohitOS Activated")
 
 
-while assistant_running:
+# -----------------------------------
+# MAIN LOOP
+# -----------------------------------
 
-    command = listen()
+while True:
 
-    if command:
+    # --------------------------------
+    # SHUTDOWN CHECK
+    # --------------------------------
 
-        response = route_command(command)
+    if is_shutdown():
 
-        # PRINT RESPONSE
-        if response:
+        print("RohitOS Fully Shutdown.")
 
-            print(f"RohitOS: {response}")
+        break
 
-            # SPEAK RESPONSE
-            speak(response)
+    # --------------------------------
+    # ACTIVE MODE
+    # --------------------------------
+
+    if is_active():
+
+        command = listen()
+
+        if command:
+
+            response = route_command(command)
+
+            if response:
+
+                print(f"RohitOS: {response}")
+
+                speak(response)
+
+    # --------------------------------
+    # SLEEP MODE
+    # --------------------------------
+
+    elif is_sleeping():
+
+        print("Waiting for wake word...")
+
+        wake_command = listen()
+
+        if wake_command:
+
+            wake_command = (
+                wake_command
+                .lower()
+                .strip()
+            )
+
+            print(
+                f"Wake Check: "
+                f"{wake_command}"
+            )
+
+            # ----------------------------
+            # FLEXIBLE WAKE WORDS
+            # ----------------------------
+
+            wake_words = [
+                "rohit",
+                "jarvis",
+                "friday",
+                "wake up"
+            ]
+
+            wake_detected = False
+
+            for wake_word in wake_words:
+
+                if wake_word in wake_command:
+
+                    wake_detected = True
+
+                    break
+
+            # ----------------------------
+            # WAKE ACTIVATION
+            # ----------------------------
+
+            if wake_detected:
+
+                set_state(ACTIVE)
+
+                print(
+                    "RohitOS Activated"
+                )
+
+                speak("Yes Boss.")
